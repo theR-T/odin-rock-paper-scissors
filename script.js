85f7860ce5bfa9bb129c6abb472b73b8784b949a
TheR-T <writetoryant@gmail.com>
2026-01-10 21:55:46 -0500

    Completed project
    I accidentally went on a trance and competed the
    whole project... Will remember to make commits
    upon completion of each section next time.

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-console.log("Hello World!")
+let computerChoice = ""
+let humanChoice = ""
+
+
+function getComputerChoice() {
+    let decider = Math.random()
+
+    if (decider < 0.33) {
+        return "rock"
+    } else if (decider >= 0.33 && decider < 0.66) {
+        return "paper"
+    } 
+
+    return "scissors"
+};
+
+function getHumanChoice() {
+    return prompt("Select rock, paper, or scissors")
+};
+
+function playGame() {
+    let humanScore = 0
+    let computerScore = 0
+
+    function playRound(humanChoice,computerChoice) {
+        humanChoice = humanChoice.toLowerCase()
+        if (humanChoice === computerChoice) {
+            return console.log("It's a tie!")
+        } else if (humanChoice === "rock" && computerChoice === "scissors" 
+            || humanChoice === "paper" && computerChoice === "rock" 
+            || humanChoice === "scissors" && computerChoice === "paper") {
+            ++humanScore
+            return console.log("Human wins!")
+        }
+        ++computerScore
+        return console.log("Computer wins!")
+    };
+
+    for (let turn = 0; turn < 5; turn++) {
+        console.log(computerChoice = getComputerChoice())
+        console.log(humanChoice = getHumanChoice())
+        playRound(humanChoice, computerChoice)
+        console.log(`ROUND ${turn} RESULTS | Humans: ${humanScore} - Computer: ${computerScore}`)
+    }
+}
+
+playGame()
+
+
+
+
